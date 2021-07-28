0812b2c2647f4bf7ddbe11814e360d95db29e947
brich <brich062@gmail.com>
2021-07-28 17:59:53 -0500

    Changed addGrade function to stop dublication issue, issue fixed but values not overwritten...will continue to review

--- coursemanager/src/main/java/com/groupproject/coursemanager/repositories/GradeRepository.java
+++ coursemanager/src/main/java/com/groupproject/coursemanager/repositories/GradeRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.groupproject.coursemanager.models.Course;
 import com.groupproject.coursemanager.models.Grade;
+import com.groupproject.coursemanager.models.User;
 
 @Repository
 public interface GradeRepository extends CrudRepository<Grade, Long> {
 	List<Grade> findAll();
+	public Grade findByStudentAndCourse(User student, Course course);
 }
